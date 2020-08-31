0a9b3425d31efaed7fcb835e50196bfca5d8cd03
Marcus Vinícius Ribeiro do Nascimento <peo_manascimento@uolinc.com>
2020-08-31 07:36:49 -0300

    fix: HTTPS api request

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1'
+const baseURL = 'https://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1'
 
 const api = axios.create({ baseURL })
 
