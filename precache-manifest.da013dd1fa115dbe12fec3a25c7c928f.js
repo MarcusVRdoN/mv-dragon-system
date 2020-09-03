2ec5d8a4eebc65058d83faaab39ef0c701264840
Marcus Vinícius Ribeiro do Nascimento <peo_manascimento@uolinc.com>
2020-09-03 02:32:01 -0300

    Updates

---
+++ precache-manifest.da013dd1fa115dbe12fec3a25c7c928f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff93de697fa3a206be073c94b4a63eeb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e75aa4e561d5ec7a953d",
+    "url": "./static/css/main.c39f0e4d.chunk.css"
+  },
+  {
+    "revision": "c922300134e7e55443b4",
+    "url": "./static/js/2.003085c9.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "./static/js/2.003085c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e75aa4e561d5ec7a953d",
+    "url": "./static/js/main.43790a73.chunk.js"
+  },
+  {
+    "revision": "3b212c6776320c26a15a",
+    "url": "./static/js/runtime-main.f5b2ee83.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mv-dragon-system/precache-manifest.86038ca555465d2e6c2da6549debc6b1.js"
+  "./precache-manifest.da013dd1fa115dbe12fec3a25c7c928f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mv-dragon-system/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
